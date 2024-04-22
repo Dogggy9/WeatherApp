13fee565e647537e2d3a8392f5cfe27057735d7b
Dogggy9 <91497727+Dogggy9@users.noreply.github.com>
2024-04-22 09:55:26 +0300

    pochty

--- app/src/main/java/com/doggy/weatherapp/MainActivity.java
+++ app/src/main/java/com/doggy/weatherapp/MainActivity.java
@@ -29,7 +29,6 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private EditText user_field;
-    private Button main_btn;
     private TextView result_info;
 
     @Override
@@ -43,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         user_field = findViewById(R.id.user_field);
-        main_btn = findViewById(R.id.main_btn);
+        Button main_btn = findViewById(R.id.main_btn);
         result_info = findViewById(R.id.result_info);
 
         main_btn.setOnClickListener(new View.OnClickListener() {
@@ -54,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     String key = "8c5097cd346ae66c846bd74e5c0f8fac";
                     String city = user_field.getText().toString();
-                    String lat_lon = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&&appid=" + key;
-                    //String lat = "";
-                    //String lon = "";
+                    //String lat_lon = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&&appid=" + key;
+                    String lat = "64.563385";
+                    String lon = "39.823769";
 
-                    new GetGPSData().execute(lat_lon);
-                    //String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=metric&lang=ru";
-                    //new GetURLData().execute(url);
+                    //new GetGPSData().execute(lat_lon);
+                    String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=metric&lang=ru";
+                    new GetURLData().execute(url);
                     //https://api.openweathermap.org/data/2.5/weather?lat=64.563385&lon=39.823769&appid=8c5097cd346ae66c846bd74e5c0f8fac
                 }
             }
@@ -121,76 +120,6 @@ public class MainActivity extends AppCompatActivity {
             try {
                 JSONObject jsonObject = new JSONObject(result);
                 result_info.setText("Температура: " + jsonObject.getJSONObject("main").getDouble("temp"));
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-
-            //result_info.setText(result);
-        }
-    }
-
-    private class GetGPSData extends AsyncTask<String, String, String>{
-
-        protected void onPreExecute(){
-            super.onPreExecute();
-            result_info.setText("Ожидайте...");
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(strings[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null)
-                    buffer.append(line).append("\n");
-                return buffer.toString();
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                if (connection != null)
-                    connection.disconnect();
-
-                try {
-                    if (reader != null)
-                        reader.close();
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(String result){
-            super.onPostExecute(result);
-
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                String key = "8c5097cd346ae66c846bd74e5c0f8fac";
-                String lat = "";
-                String lon = "";
-                lat = String.valueOf(jsonObject.getJSONObject("lat"));
-                lat = String.valueOf(jsonObject.getDouble("lat"));
-
-                lon = String.valueOf(jsonObject.getJSONObject("lon"));
-                //String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=metric&lang=ru";
-                //new GetURLData().execute(url);
-                //result_info.setText(lat);
-
-                //result_info.setText("Температура: " + jsonObject.getJSONObject("main").getDouble("temp"));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
